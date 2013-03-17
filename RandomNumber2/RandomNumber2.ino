4b85aff11bb8785b317813bae701b7cd6ca65ad6
Tom Opgenorth <tom@opgenorth.net>
2013-03-17 10:34:05 -0600

    Log message to file on disk and to Serial.

--- RandomNumber2/RandomNumber2.ino
+++ RandomNumber2/RandomNumber2.ino
@@ -5,33 +5,69 @@
  * March 3rd, 2013
  */
 
+#include <SD.h>
 
 const unsigned int LED_BIT0=2;
 const unsigned int LED_BIT1=3;
-const unsigned int LED_BIT2=4;
+const unsigned int SD_CARD_PIN=4;
+const unsigned int LED_BIT2=5;
 const unsigned int BUTTON_PIN=7;
+const unsigned int SD_CARD_OUTPUT_PIN=10;
 
 const unsigned int PAUSE=2000;
 const unsigned int BAUD_RATE = 9600;
 const unsigned int DEBOUNCE_DELAY=50;
 
-
+File _logFile;
 int current_value = 0;
 int old_value = 0;
 
 void setup ()
 {
-	Serial.begin(BAUD_RATE);    
+	initializeLogging();
 	randomSeed(analogRead(A0));
 
 	pinMode(LED_BIT0, OUTPUT);
 	pinMode(LED_BIT1, OUTPUT);
 	pinMode(LED_BIT2, OUTPUT);
-
 	pinMode(BUTTON_PIN, INPUT);
 
-	Serial.println("RandomNumber2 - setup complete.");
+	logMessage("RandomNumber2 - setup complete.");
 	turn_off_all_LED();
+}
+
+void initializeLogging() 
+{
+	Serial.begin(BAUD_RATE);    
+
+	// Initialize the SD card.
+	pinMode(SD_CARD_OUTPUT_PIN, OUTPUT);
+	if (!SD.begin(SD_CARD_PIN)) 
+	{
+		Serial.println("Could not initialize SD card.");
+	}
+	_logFile = SD.open("rannum2.txt", FILE_WRITE);
+	if (_logFile)
+	{
+		_logFile.println("Initialized.");
+		_logFile.close();
+	} 
+	else
+	{
+		Serial.println("Could not open the file rannum2.txt");
+	}	
+}
+
+void logMessage(String message) 
+{
+	Serial.println(message);
+
+	_logFile = SD.open("rannum2.txt", FILE_WRITE);
+	if (_logFile)
+	{
+		_logFile.println(message);
+		_logFile.close();
+	}
 }
 
 void loop()
@@ -52,7 +88,10 @@ void loop()
  */
 void output_result(const long result) 
 {
-	Serial.print("showing number ");
+	String message = "Display number: ";
+	message += result;
+	logMessage(message);
+	
 	Serial.println(result);
 	switch (result) 
 	{
@@ -79,5 +118,5 @@ void turn_off_all_LED()
 	digitalWrite(LED_BIT0, LOW);
 	digitalWrite(LED_BIT1, LOW);
 	digitalWrite(LED_BIT2, LOW);
-	Serial.println("Turned off all the LEDs.");
+	logMessage("Turn off all LEDs.");
 }
