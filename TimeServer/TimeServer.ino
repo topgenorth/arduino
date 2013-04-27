510dc3ba5149029893565554b5b232c12336e016
Tom Opgenorth <tom@opgenorth.net>
2013-04-27 15:38:40 -0600

    Code cleanup.

--- TimeServer/TimeServer.ino
+++ TimeServer/TimeServer.ino
@@ -1,14 +1,18 @@
+/************************************************************************
+ * 
+ * Just a simple little sketch that will obtain an IP address via 
+ * DHCP and then contact the time server at time.nist.gov
+ * 
+ * April 27, 2013
+ *
+ ************************************************************************/
 #include <SPI.h>
 #include <Ethernet.h>
 
 const unsigned int BAUD_RATE = 9600;
 const unsigned int DAYTIME_PORT = 13;
 const char* time_server = "time.nist.gov";
-
-byte ip[] = {192,168,1,177}; 						// ip address for ethernet shield
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED}; // max address for ethernet shield
-int networkConfirmed = 0;
-char buffer [256];
 EthernetClient client;
 
 void setup() {
@@ -24,7 +28,8 @@ void setup() {
 }
 
 void loop() {
-	delay(1000);
+	delay(5000);
+	Serial.println();
 	Serial.print("Connecting...");
 	if (client.connect(time_server, DAYTIME_PORT) <= 0) {
 		Serial.println("connection failed.");
@@ -40,11 +45,12 @@ void loop() {
 
 	Serial.println("Disconnecting.");
 	client.stop();
+	Serial.println();
 }
 
 void print_ip_address(IPAddress ip) {
 	const unsigned int OCTETS = 4;
-	Serial.print("We've got the following IP address:");
+	Serial.print("My IP address:");
 	for (unsigned int i = 0; i < OCTETS; i++) {
 		Serial.print(ip[i]);
 		if (i != OCTETS - 1) {
