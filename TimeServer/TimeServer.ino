8ee65a1fe465e79120edfe5071b51c327644b4ec
Tom Opgenorth <tom@opgenorth.net>
2013-04-27 15:23:22 -0600

    Get IP address via DHCP, connect to time.nist.gov.

--- TimeServer/TimeServer.ino
+++ TimeServer/TimeServer.ino
@@ -1,137 +1,56 @@
-/*
- *
- * DS1307 RTC library : https://code.google.com/p/ds1307new/
- */
-
 #include <SPI.h>
 #include <Ethernet.h>
-#include <DS1307new.h>
 
 const unsigned int BAUD_RATE = 9600;
 const unsigned int DAYTIME_PORT = 13;
+const char* time_server = "time.nist.gov";
 
-IPAddress my_ip(192,168,1,112);
-IPAddress time_server(192,43,244,18); // time.nist.gov
+byte ip[] = {192,168,1,177}; 						// ip address for ethernet shield
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED}; // max address for ethernet shield
+int networkConfirmed = 0;
+char buffer [256];
 EthernetClient client;
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 
 void setup() {
 	Serial.begin(BAUD_RATE);
-	Ethernet.begin(mac, my_ip);
+	if (Ethernet.begin(mac) == 0) {
+		for (;;) {
+			Serial.println("Could not obtain IP address.");
+			delay(1000);
+		}
+	} else {
+		print_ip_address(Ethernet.localIP());
+	}
 }
 
 void loop() {
 	delay(1000);
-	// show_clock();
-	connect_to_time_server();
-}
-
-void connect_to_time_server() {
 	Serial.print("Connecting...");
 	if (client.connect(time_server, DAYTIME_PORT) <= 0) {
 		Serial.println("connection failed.");
 	} else {
 		Serial.println("connected.");
 		delay(300);
-
-		while(client.available() ) {
-			char c = client.read();
-			Serial.print(c);
-		}
-
-		Serial.println("disconnecting.");
-		client.stop();
 	}
+
+	while (client.available()) {
+		char c = client.read();
+		Serial.print(c);
+	}
+
+	Serial.println("Disconnecting.");
+	client.stop();
 }
 
-/*
-void show_clock() {
-	Serial.print("Today's date is ");
-	RTC.getTime();
-
-	switch (RTC.dow) {
-		case 1:
-			Serial.print("Monday");
-			break;
-		case 2:
-			Serial.print("Tuesday");
-			break;
-		case 3:
-			Serial.print("Wednesday");
-			break;
-		case 4:
-			Serial.print("Thursday");
-			break;
-		case 5:
-			Serial.print("Friday");
-			break;
-		case 6:
-			Serial.print("Saturday");
-			break;
-		case 7:
-			Serial.print("Sunday");
-			break;
+void print_ip_address(IPAddress ip) {
+	const unsigned int OCTETS = 4;
+	Serial.print("We've got the following IP address:");
+	for (unsigned int i = 0; i < OCTETS; i++) {
+		Serial.print(ip[i]);
+		if (i != OCTETS - 1) {
+			Serial.print(".");
+		}
 	}
 
-	Serial.print(", ");
-	switch (RTC.month) {
-		case 1:
-			Serial.print("January");
-			break;
-		case 2:
-			Serial.print("February");
-			break;
-		case 3:
-			Serial.print("March");
-			break;
-		case 4:
-			Serial.print("April");
-			break;
-		case 5:
-			Serial.print("May");
-			break;
-		case 6:
-			Serial.print("June");
-			break;
-		case 7:
-			Serial.print("July");
-			break;
-		case 8:
-			Serial.print("August");
-			break;
-		case 9:
-			Serial.print("September");
-			break;
-		case 10:
-			Serial.print("October");
-			break;
-		case 11:
-			Serial.print("November");
-			break;
-		case 12:
-			Serial.print("December");
-			break;
-	}
-
-	Serial.print(" ");
-	Serial.print(RTC.day, DEC);
-	Serial.print(", ");
-	Serial.println(RTC.year, DEC);
-
-	Serial.print("The Current Time is ");
-	if (RTC.hour < 10) {
-		Serial.print("0");
-		Serial.print(RTC.hour, DEC);
-	} else {
-		Serial.print(RTC.hour, DEC);
-	}
-	Serial.print(":");
-	if (RTC.minute < 10) {
-		Serial.print("0");
-		Serial.print(RTC.minute, DEC);
-	} else {
-		Serial.print(RTC.minue, DEC);
-	}
 	Serial.println();
 }
-*/
