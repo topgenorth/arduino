193af02c37e725b1969b100f276e94f29a74421b
Tom Opgenorth <tom@opgenorth.net>
2013-07-01 21:34:16 -0600

    Update .gitignore.

--- TempLogger/TempLogger.ino
+++ TempLogger/TempLogger.ino
@@ -1,33 +1,50 @@
 /**
  *
  */
-#include <SD.h>
 #include <SPI.h>
 #include <Ethernet.h>
+#include <HttpClient.h>
+#include <Xively.h>
+#include "Secrets.h"  // Xively API key is defined in this file.
 
-const unsigned int BAUD_RATE              = 9600;
-const unsigned int DHCP_MAX_ATTEMPT_COUNT = 5;
+const unsigned int BAUD_RATE               = 9600;
+const byte DHCP_MAX_ATTEMPT_COUNT          = 5;
 
 // Millisecond delays, depending on the context
-const unsigned long IP_DELAY              = 5000;     // The delay while trying to get the IP address via DHCP.
-const unsigned short IP_DELAY_SECONDS     = IP_DELAY / 1000;
+const unsigned int IP_DELAY                = 5000;     // The delay while trying to get the IP address via DHCP.
+const unsigned int IP_DELAY_SECONDS        = IP_DELAY / 1000;
 
-//const unsigned long CONNECTION_INTERVAL   = 60000;    // Only poll the sensor every 10 minutes.
-const unsigned long CONNECTION_INTERVAL   = 1000; 
+//const unsigned long CONNECTION_INTERVAL    = 60000;    // Only poll the sensor every 10 minutes.
+const unsigned int CONNECTION_INTERVAL     = 1000; 
 
 // Pins in use by this sketch.
-const unsigned int TEMP_SENSOR_PIN        = 3;        // The temperature sensor pin is A0
-const unsigned int CS_PIN                 = 4;        // Required by the Ethernet shield
-const unsigned int SDCARD_PIN             = 10;       // Required by the Ethernet shield
+const byte TEMP_SENSOR_PIN                 = 3;        // The temperature sensor pin is A0
+const byte CS_PIN                          = 4;        // Required by the Ethernet shield
+const byte SDCARD_PIN                      = 10;       // Required by the Ethernet shield
 
 // Constants for converting the TMP36 readings into degress Celsius
-const float TEMPERATURE_ADJUSTMENT        = -2.7;     // Use this value to correct the reading of the TMP_36 sensor.
-const float SUPPLY_VOLTAGE                = 5000;     // milliVolts
+const float DEFAULT_TEMPERATURE_ADJUSTMENT = -2.7;     // Use this value to correct the reading of the TMP_36 sensor.
+const int SUPPLY_VOLTAGE                   = 5000;     // milliVolts
+float temperatureAdjustment                = 0;
 
 // Ethernet variables.
 EthernetClient client;
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; // MAC address for your Ethernet shield
-unsigned int initializedEthernet = 0;
+int initializedEthernet                    = 0;
+byte mac[] = { 
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; // MAC address for your Ethernet shield
+
+// Xively stuff
+char sensorID[]                            = "tmp36_sensor_reading";
+char temperatureID[]                       = "tmp36_temperature";
+char temperatureAdjustmentID[]             = "tmp36_temperature_adjustment";
+
+XivelyDatastream datastreams[] = {
+  XivelyDatastream(sensorID, strlen(sensorID), DATASTREAM_FLOAT),
+  XivelyDatastream(temperatureID, strlen(temperatureID), DATASTREAM_FLOAT),
+  XivelyDatastream(temperatureAdjustmentID, strlen(temperatureAdjustmentID), DATASTREAM_FLOAT),
+};
+XivelyFeed feed(XIVELY_FEED_ID, datastreams, 2);
+XivelyClient xivelyclient(client);
 
 void setup() {
   delay(250);
@@ -36,9 +53,8 @@ void setup() {
   Serial.println();
   Serial.println("********** SETUP     **********");
 
-  // Always initialize the SDCard first.
-  init_sdcard();  
   init_ethernet();
+  init_temperature_adjustment() ;
 
   Serial.println();
   Serial.println("********** SETUP COMPLETE *****");    
@@ -50,12 +66,36 @@ void loop() {
   delay(CONNECTION_INTERVAL);
 }
 
-void init_ethernet() {
+void init_temperature_adjustment() {  
+  if (initializedEthernet) {
+    Serial.print("Contacting xively...");
+    int getReturn = xivelyclient.get(feed, XIVELY_API_KEY);
+
+    if(getReturn > 0){
+      temperatureAdjustment = feed[2].getFloat();
+      Serial.print("got temperature adjustment : ");
+      Serial.print(temperatureAdjustment);
+    }
+    else {
+      Serial.print("HTTP Error");
+      Serial.print(getReturn);
+    }
+  }
+  else {
+    temperatureAdjustment = DEFAULT_TEMPERATURE_ADJUSTMENT;
+    Serial.print("No internet connection, can't read temperature adjustment value");
+  }
+  
+  Serial.println(".");
+
+  return;
+}
+
+int init_ethernet() {
   int tryToInitializeEthernet = 1;
   int initCount               = 0;
-  int intializedEthernet      = 0;
   const unsigned short SECONDS = IP_DELAY / 1000;
-  
+
   Serial.println("Initializing Ethernet.");
 
   while (tryToInitializeEthernet) {
@@ -63,7 +103,7 @@ void init_ethernet() {
     Serial.print("Attempt #");
     Serial.print(initCount);
     Serial.print("...");
-    
+
     if (Ethernet.begin(mac) == 1) {
       initializedEthernet = 1;
       tryToInitializeEthernet = 0;
@@ -97,12 +137,12 @@ void init_ethernet() {
   }
 
   Serial.println();
-  return;
+  return initializedEthernet;
 }
 
 void print_ip_address(IPAddress ipAddress) {
   const unsigned int OCTETS = 4;
-  Serial.print("Ethernet initialized, IP address:");
+  Serial.print("Ethernet initialized, IP address: ");
   for (unsigned int i=0; i<OCTETS; i++) {
     Serial.print(ipAddress[i]);
     if (i != OCTETS - 1) {
@@ -115,9 +155,10 @@ void print_ip_address(IPAddress ipAddress) {
 void read_temperature_value() {
   const int sensorValue = analogRead(TEMP_SENSOR_PIN);
   const float milliVolts = sensorValue * SUPPLY_VOLTAGE / 1024.0;
-  const float temperature = ((milliVolts - 500) / 10) + TEMPERATURE_ADJUSTMENT;  
-  
+  const float temperature = ((milliVolts - 500) / 10) + DEFAULT_TEMPERATURE_ADJUSTMENT;  
+
   log_temperature(sensorValue, milliVolts, temperature);
+  send_to_xively(sensorValue, temperature);
 }
 
 void log_temperature(int sensorValue, float milliVolts, float temperature) {
@@ -131,20 +172,12 @@ void log_temperature(int sensorValue, float milliVolts, float temperature) {
   Serial.print(" Centigrade");
 }
 
-void init_sdcard() {
-  delay(150);
-  Serial.print("Initializing SD card...");
-  pinMode(SDCARD_PIN, OUTPUT);
-  if(!SD.begin(CS_PIN)) {
-    Serial.println("initialization failed!");
-    return;
-  }
+void send_to_xively(int sensorValue, float temperature) {
+  datastreams[0].setFloat(sensorValue);
+  //  datastreams[1].setFloat(temperature);
+  int ret = xivelyclient.put(feed, XIVELY_API_KEY);
 
-  Serial.println("initialization succeded.");
-  File logFile= SD.open("readtemp.txt", FILE_WRITE);
-  if (logFile) {
-    logFile.println("Starting.");
-    logFile.close();
-  }  
+  //  Serial.print("Uploaded data to Xively, ");
+  //  Serial.println(ret);
 }
 
