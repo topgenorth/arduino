d62070f11e70533777fd41d997bfb8f2a78a9d0c
Tom Opgenorth <tom@opgenorth.net>
2013-10-14 10:07:52 -0600

    Blink LED's depending on the perceived temperature.

--- TempLogger/TempLogger.ino
+++ TempLogger/TempLogger.ino
@@ -2,16 +2,22 @@
  *
  */
 const unsigned int BAUD_RATE               = 9600;
-const unsigned int READING_DELAY           = 60000;
+const unsigned int READING_DELAY           = 5000;
 
 // Pins in use by this sketch.
+const byte HOT_PIN                         = 2;        // The LED for when it's to warm
 const byte TEMP_SENSOR_PIN                 = 3;        // The temperature sensor pin is A0
 const byte CS_PIN                          = 4;        // Required by the Ethernet shield
+const byte NORMAL_PIN                      = 5;        // The LED for when the temperature is OK
+const byte COLD_PIN                        = 6;        // The LED for it's to cool/cold
 const byte SDCARD_PIN                      = 10;       // Required by the Ethernet shield
 
+
 // Constants for converting the TMP36 readings into degress Celsius
-const float DEFAULT_TEMPERATURE_ADJUSTMENT = -2.7;     // Use this value to correct the reading of the TMP_36 sensor.
-const float SUPPLY_VOLTAGE                 = 5000;      // Volts, not milliVolts
+const float DEFAULT_TEMPERATURE_ADJUSTMENT = -29.5;     // Use this value to correct the reading of the TMP_36 sensor.
+const float SUPPLY_VOLTAGE                 = 5000;     // Volts, not milliVolts
+const float HOT_TEMP                       = 24;
+const float COOL_TEMP                      = 17;
 
 int firstReading = 1;
 void setup() {
@@ -21,19 +27,27 @@ void setup() {
   Serial.println();
   Serial.println("********** SETUP     **********");
 
+  pinMode(HOT_PIN, OUTPUT);
+  pinMode(NORMAL_PIN, OUTPUT);
+  pinMode(COLD_PIN, OUTPUT);
+
   Serial.println("Looks like we're all initialized.");
 
   Serial.println();
   Serial.println("********** SETUP COMPLETE *****");    
 }
 
+
 void loop() {
   Serial.println();
-  read_temperature_value();
+  float temperature = read_temperature_value();
+  
+  turn_on_led_for(temperature);
+
   delay(READING_DELAY);
 }
 
-void read_temperature_value() {
+float read_temperature_value() {
   int sensorValue = analogRead(TEMP_SENSOR_PIN);
   if (firstReading) {
     // we discard the first reading - give things a chance to settle down.
@@ -43,6 +57,29 @@ void read_temperature_value() {
   }
   float temperature = convert_sensor_reading_to_celsius(sensorValue);
   log_temperature(sensorValue, temperature);
+
+  return temperature;
+}
+
+void turn_on_led_for(float temperature) {
+  if (temperature <= COOL_TEMP) {
+    digitalWrite(COLD_PIN, HIGH);
+    digitalWrite(HOT_PIN, LOW);
+    digitalWrite(NORMAL_PIN, LOW);
+    // Serial.println("  Turn on cool LED.");
+  }
+  else if (temperature > COOL_TEMP && temperature < HOT_TEMP) {
+    digitalWrite(COLD_PIN, LOW);
+    digitalWrite(HOT_PIN, LOW);
+    digitalWrite(NORMAL_PIN, HIGH);
+    // Serial.println("  Turn on normal LED.");
+  }
+  else {
+    digitalWrite(COLD_PIN, LOW);
+    digitalWrite(HOT_PIN, HIGH);
+    digitalWrite(NORMAL_PIN, LOW);
+    // Serial.println("  Turn on hot LED.");
+  }
 }
 
 float convert_sensor_reading_to_celsius(int sensor_value) {
