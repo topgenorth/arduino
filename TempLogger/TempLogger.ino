0f6d83a0f1c5b23abc941ac012246490d3ff513c
Tom Opgenorth <tom@opgenorth.net>
2013-10-19 14:14:31 -0600

    Some changes/tweaks to accommodate the Arduino Mega ADK board.

--- TempLogger/TempLogger.ino
+++ TempLogger/TempLogger.ino
@@ -1,9 +1,13 @@
 /**
  * Simple sketch to read the temperature, and then display a red/green/yellow light for 
  * to hot/just right/ to cold. Will also write the temperature to a CSV on the SDcard.
+ * 
  */
 
  #include "SD.h"
+
+// Set this to 0 for the Uno R3, 1 for the Mega ADK board.
+ #define MEGA_ADK 1
 
  const unsigned int BAUD_RATE               = 9600;
  const unsigned int READING_DELAY           = 30000;
@@ -16,12 +20,15 @@ const byte NORMAL_PIN                      = 5;        // The LED for when the t
 const byte COLD_PIN                        = 6;        // The LED for it's to cool/cold
 const byte SDCARD_PIN                      = 10;       // Required by the Ethernet shield
 
-
 // Constants for converting the TMP36 readings into degress Celsius
 const float SUPPLY_VOLTAGE                 = 5000;     // Volts, not milliVolts
 const float HOT_TEMP                       = 24;
 const float COOL_TEMP                      = 15;
+#if MEGA_ADK 
 const int TMP36_ADJUSTMENT                 = -60;      // My TMP36 seems to be damaged/mis-calibrated. This should compensate? 
+#else
+const int TMP36_ADJUSTMENT                 = -167;
+#endif
 
 File tempFile;
 int readCount = 0;
@@ -36,6 +43,7 @@ void setup() {
   init_led();
   init_sdcard();
   Serial.println("********** SETUP COMPLETE *****");    
+  delay(2000);
 }
 
 void init_led() {
@@ -47,15 +55,24 @@ void init_led() {
 void init_sdcard() {
   Serial.print("Initializing SD card...");
 
+#if MEGA_ADK
+  // Disable w5100 while setting up SD
   pinMode(SDCARD_PIN, OUTPUT);
-  
+  digitalWrite(SDCARD_PIN, HIGH);
+#endif
+
+  // Setup SD card...
   if (!SD.begin(CS_PIN)) {
     Serial.println("failed!");
     return;
   }
 
   Serial.println("succeded.");
+#if MEGA_ADK
+  tempFile = SD.open("TEMPMEGA.CSV", FILE_WRITE);
+#else
   tempFile = SD.open("TEMP.CSV", FILE_WRITE);
+#endif
   if (tempFile) {
     tempFile.close();
   }
@@ -63,10 +80,17 @@ void init_sdcard() {
     Serial.println("Failed to connnect to SD card.");
     writeToTemperatureFile=0;
   }
+
+  // Setup up w5100
+  // Ethernet.begin(mac,ip);
+  // Disable w5100 SPI
+  digitalWrite(SDCARD_PIN, HIGH);
+
+  // It takes a bit of time for the w5100 to get going
+  delay(2000);
 }
 
 void loop() {
-  Serial.println();
   float temperature = read_temperature_value();
   
   turn_on_led_for(temperature);
@@ -74,7 +98,7 @@ void loop() {
   delay(READING_DELAY);
 }
 
-float read_temperature_value() {
+const float read_temperature_value() {
   int sensorValue = analogRead(TEMP_SENSOR_PIN);
   while (readCount < 3) {
     // we discard the first three reading or so
@@ -82,7 +106,7 @@ float read_temperature_value() {
     sensorValue = analogRead(TEMP_SENSOR_PIN);
     readCount++;
   }
-  float temperature = convert_sensor_reading_to_celsius(sensorValue);
+  const float temperature = convert_sensor_reading_to_celsius(sensorValue);
   log_temperature(sensorValue, temperature);
 
   return temperature;
@@ -106,7 +130,7 @@ void turn_on_led_for(float temperature) {
   }
 }
 
-float convert_sensor_reading_to_celsius(int sensor_value) {
+float convert_sensor_reading_to_celsius(const int sensor_value) {
   const int adjusted_sensor_value= sensor_value + TMP36_ADJUSTMENT; 
   const float volts = (adjusted_sensor_value * SUPPLY_VOLTAGE) / 1024;
   const float adjustedVoltage = (volts - 500);  
@@ -114,16 +138,20 @@ float convert_sensor_reading_to_celsius(int sensor_value) {
   return temperature;
 }
 
-void log_temperature(int sensorValue, float temperature) {
+void log_temperature(const int sensorValue, const float temperature) {
   Serial.print(sensorValue);
   Serial.print(",");
   Serial.println(temperature);
   write_values_to_csv(sensorValue, temperature);
 }
 
-void write_values_to_csv(int sensorValue, float temperature) {
+void write_values_to_csv(const int sensorValue, const float temperature) {
   if (writeToTemperatureFile) {
-    tempFile = SD.open("TEMP.CSV", FILE_WRITE);
+    #if MEGA_ADK
+    tempFile = SD.open("TEMPMEGA.CSV", FILE_WRITE);
+    #else
+    tempFile = SD.open("MEGA.CSV", FILE_WRITE);
+    #endif 
     if (tempFile) {
       tempFile.print(sensorValue);
       tempFile.print(",");
@@ -131,7 +159,7 @@ void write_values_to_csv(int sensorValue, float temperature) {
       tempFile.close();
     }
     else {
-      Serial.println("Failed to open TEMP.CSV, cannot write data to SD card.");
+      Serial.println("Failed to open CSV file, cannot write data to SD card.");
       writeToTemperatureFile = 0;
     }
   }  
